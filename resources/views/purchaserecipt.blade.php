7af2f0d88098b883c5cc567ec84708828fba342a
Shubham Kumar <meshubham1995@gmail.com>
2022-05-12 20:00:36 +0530

    -

--- resources/views/purchaserecipt.blade.php
+++ resources/views/purchaserecipt.blade.php
@@ -1,25 +1,30 @@
-@extends('layouts.app')
-@push('page-css')
-<!-- Select2 css-->
-<link rel="stylesheet" href="{{asset('assets/plugins/select2/css/select2.min.css')}}">
-<style type="text/css" media="print">
-   @media print {
-     @page {
-        margin-left: 0.5in;
-        margin-right: 0.5in;
-        margin-top: 0;
-        margin-bottom: 0;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title> </title>
+    <meta charset="UTF-8" />
+    <style>
+      td {
+        vertical-align: top;
+        padding: 5px;
       }
-    }
-    @media print {
-    .printBtn {
-        display: none;
-    }
-    }
-</style>
-@endpush
 
-<?php
+      td td {
+        padding: 0;
+      }
+      @media print {
+        #printPageButton {
+            display: none;
+        }
+        #backButton {
+            display: none;
+        }
+      }
+    </style>
+  </head>
+
+  <body style="font-size: 14px; font-family: arial;">
+  <?php
 
 function getIndianCurrency(float $number)
 {
@@ -73,160 +78,136 @@ function getIndianCurrency(float $number)
 }
 
 ?>
+<a href="{{route('addorder')}}" id="backButton" class="btn btn-success text-center backBtn">Back</a></br>
+    <div style="max-width: 800px; width: 100%; margin: 0 auto;">
+      <table style="border-collapse: collapse;width: 100%;">
+        <tr>
+          <td colspan="2" align="center" style="font-size: 30px;">
+            <strong>Rampari Ausadhalaya</strong>
+            <h5 style="font-size: 15px;margin-top: 3px;">GSTIN NO - 10BCLPK9104P1ZE</h5>
+            <h5 style="font-size: 15px;margin-top: -15px;">DL NO - 150308/309</h5>
+            <h5 style="font-size: 15px;margin-top: -15px;">(A Unit of Shree Pradhan Healthcare Pvt Ltd)</h5>
+            <h5 style="font-size: 15px;margin-top: -15px;">N.H-28, Khabra Muzaffarpur, Bihar-843136</h5>
+            <h5 style="font-size: 15px;margin-top: -15px;">Email:shreepradhanhospital@gmail.com | Ph. 9199654999</h5>
+          </td>
+        </tr>
+        <tr>
+          <td style="border: 1px solid;">
+            <table>
+              <tr>
+                <td><strong>Customer Name</strong></td>
+                <td>: <strong>{{$customer->name}}</strong></td>
+              </tr>
 
-<a href="{{route('addorder')}}" class="btn btn-success text-center backBtn">Back</a></br>
-@section('content')
-<div style="max-width: 800px; width: 100%; margin: 0 auto;">
-        <table style="border-collapse: collapse;width: 100%;">
-            <tr>
-                <td colspan="2" align="center" style="font-size: 30px;">
-                    <strong>Rampari Ausadhalaya</strong>
-                    <h5 style="font-size: 15px;margin-top: 2px;"><strong>GSTIN NO - 10BCLPK9104P1ZE</strong></h5>
-                    <h5 style="font-size: 15px;margin-top: 2px;"><strong>DL NO - 150308/309</strong></h5>
-                    <h5 style="font-size: 15px;margin-top: 2px;"><strong>(A Unit of Shree Pradhan Healthcare Pvt Ltd)</strong></h5>
-                    <h5 style="font-size: 15px;margin-top: 2px;"><strong>N.H-28, Khabra Muzaffarpur, Bihar-843136</strong></h5>
-                    <h5 style="font-size: 15px;margin-top: 2px;"><strong>Email:shreepradhanhospital@gmail.com | Ph. 9199654999</strong></h5>
-                </td>
-            </tr>
-            <tr>
-                <td style="border: 1px solid;padding: 10px;">
-                    <table>
-                        <tr>
-                            <td><strong>Name</strong></td>
-                            <td>: <strong>{{$customer->name}} </strong></td>
-                        </tr>
-                        <tr>
-                            <td><strong>Doctor Name:</strong></td>
-                            <td>: <strong>{{$customer->doctor_name}}</strong></td>
-                        </tr>
-                        @if(!empty($customer->ipd_id))
-                        <tr>
-                            <td><strong>IPD No</strong></td>
-                            <td>: <strong>{{$customer->ipd_id}} </strong></td>
-                        </tr>
-                        @endif
-                    </table>
-                </td>
-                <td style="border: 1px solid;padding: 10px;">
-                    <table>
-                        <tr>
-                            <td><strong>Bill No</strong></td>
-                            <td>: <strong>{{$customer->order__id}} </strong></td>
-                        </tr>
-                        <tr>
-                            <td><strong>Bill Date:</strong></td>
-                            <td>: <strong>{{date('d-m-Y',strtotime($customer->created_at))}}</strong></td>
-                        </tr>
-                        <tr>
-                            <td><strong>Bill Type</strong></td>
-                            <td>: <strong>{{ucwords($customer->sale_type)}} </strong></td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-        </table>
+              <!-- <tr>
+                <td><strong>Mobile No.</strong></td>
+                <td>: +91 9948473495</td>
+              </tr> -->
 
-            <table style="border-collapse: collapse;width: 100%; border: 1px solid; text-align: center;">+
+              <!-- <tr>
+                <td><strong>Doctor Name</strong></td>
+                <td>: {{$customer->doctor_name}}</td>
+              </tr> -->
+              @if(!empty($customer->ipd_id))
+                <tr>
+                    <td><strong>IPD No</strong></td>
+                    <td>: <strong>{{$customer->ipd_id}} </strong></td>
+                </tr>
+              @endif
+              <tr>
+                <td><strong>Bill Date:</strong></td>
+                <td>: <strong>{{date('d-m-Y',strtotime($customer->created_at))}}</strong></td>
+              </tr>
+            </table>
+          </td>
+          <td  style="border: 1px solid;">
+            <table>
+            <tr>
+                <td><strong>Doctor Name</strong></td>
+                <td>: {{$customer->doctor_name}} </td>
+              </tr>
+              <tr>
+                <td><strong>Bill No</strong></td>
+                <td>: {{$customer->order__id}} </td>
+              </tr>
+              <tr>
+                <td><strong>Bill Type</strong></td>
+                <td>: {{ucwords($customer->sale_type)}} </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
 
-            <tr>
-                <td style="border-bottom: 1px solid;"><strong>S.No.</strong></td>
-                <td style="border-bottom: 1px solid;"><strong>Medicine Category</strong></td>
-                <td style="border-bottom: 1px solid;"><strong>Medicine Name</strong></td>
-                <td style="border-bottom: 1px solid;"><strong>Price</strong></td>
-                <td style="border-bottom: 1px solid;"><strong>Quantity</strong></td>
-            </tr>
-            <?php $count=1; $totalamt = 0.0; ?>
-            @foreach($orderitems as $orderitem)
-            <?php $totalamt += $orderitem->total_amount; ?>
-            <tr>
-                <td><strong>{{$count++}}</strong></td>
-                <td><strong>{{$orderitem->medicine_category}}</strong></td>
-                <td><strong>{{$orderitem->medicine_name}}</strong></td>
-                <td><strong>{{round($orderitem->price)}}</strong></td>
-                <td><strong>{{$orderitem->quantity}}</strong></td>
-            </tr>
-            @endforeach
+      <table style="border-collapse: collapse;width: 100%; border: 1px solid; text-align: center;">
+        <tr>
+          <td style="border-bottom: 1px solid;"><strong>S.No.</strong></td>
+          <td style="border-bottom: 1px solid;"><strong>Medicine Category</strong></td>
+          <td style="border-bottom: 1px solid;"><strong>Medicine Name</strong></td>
+          <td style="border-bottom: 1px solid;"><strong>Price</strong></td>
+          <td style="border-bottom: 1px solid;"><strong>Quantity</strong></td>
+        </tr>
+        <?php $count=1; $totalamt = 0.0; ?>
+        @foreach($orderitems as $orderitem)
+        <?php $totalamt += $orderitem->total_amount; ?>
+        <tr>
+          <td>{{$count++}}</td>
+          <td style="text-align: leftt;">
+            {{$orderitem->medicine_category}}
+            <!-- <div>Bed Room Charges/ General Room</div> -->
+          </td>
+          <td>{{$orderitem->medicine_name}}</td>
+          <td>{{round($orderitem->price)}}</td>
+          <td>{{$orderitem->quantity}}</td>
+        </tr>
+        @endforeach
+        <tr>
+          <td colspan="3" rowspan="4" style="border-top:1px solid; text-align: left;">
+            <!-- <div><strong style="border-bottom: 1px solid;">Receipt Details :</strong></div>
+           <div style="padding-top: 10px;">Dr. xyz kumar yadav</div>
+           <div style="max-width: 80%; padding-top: 10px;">dslajf ldsajf; jasl;f jlksa jfdjdsaf lkjdsa f;lkdsajf ;jdsaf ;lks jfda;sjaf;l dsaf</div> -->
+          </td>
 
-            <tr>
-                <td colspan="3" rowspan="1" style="border-top:1px solid; text-align: left;">
-                    <div><strong style="border-bottom: 1px solid;"></strong></div>
-                    <div style="padding-top: 10px;"></div>
-{{--                    <div style="max-width: 80%; padding-top: 10px;">dslajf ldsajf; jasl;f jlksa jfdjdsaf lkjdsa f;lkdsajf ;jdsaf ;lks jfda;sjaf;l dsaf</div>--}}
-                </td>
+          <td style="border-top:1px solid;">Total Amount :</td>
+          <td style="border-top:1px solid;">{{getIndianCurrency($totalamt)}}</td>
+        </tr>
+        <?php $discountamt = 0;?>
+        @if($customer->discount_amount > 0)
+        <?php $discountamt = $customer->discount_amount ?>
+          <tr>
+            <td>Discount: </td>
+          <td>{{$customer->discount_amount}}</td>
+        </tr>
+        <?php $returnamt = 0; $returnamt = $customer->paid_amount - $customer->amount; ?>
+        <tr>
+          <td>Total Amount : </td>
+          <td>{{getIndianCurrency($totalamt - $customer->discount_amount)}}</td>
+        </tr>
+        @endif
+        <tr>
+          <td>Paid Amount :</td>
+          <td>{{$customer->paid_amount}}</td>
+        </tr>
+        <tr>
+          <td>Change Amount :</td>
+          <td>{{round($customer->paid_amount - $customer->amount)}}</td>
+        </tr>
+      </table>
 
-                <td style="border-top:1px solid;"><strong>Total Amount :</strong></td>
-                <td style="border-top:1px solid;"><strong>{{getIndianCurrency($totalamt)}}</strong></td>
-            </tr>
-            <?php $discountamt = 0;?>
-            @if($customer->discount_amount > 0)
-            <?php $discountamt = $customer->discount_amount ?>
-            <tr>
-                <td colspan="3" rowspan="1" style="border-top:1px solid; text-align: left;">
-                    <div><strong style="border-bottom: 1px solid;"></strong></div>
-                    <div style="padding-top: 10px;"></div>
-{{--                    <div style="max-width: 80%; padding-top: 10px;">dslajf ldsajf; jasl;f jlksa jfdjdsaf lkjdsa f;lkdsajf ;jdsaf ;lks jfda;sjaf;l dsaf</div>--}}
-                </td>
+      <table style="width: 100%;">
+        <tr>
+          <td style="height: 60px;"></td>
+        </tr>
+        <tr>
 
-                <td style="border-top:1px solid;"><strong>Discount :</strong></td>
-                <td style="border-top:1px solid;"><strong>{{$customer->discount_amount}}</strong></td>
-            </tr>
-            <tr>
-                <td colspan="3" rowspan="1" style="border-top:1px solid; text-align: left;">
-                    <div><strong style="border-bottom: 1px solid;"></strong></div>
-                    <div style="padding-top: 10px;"></div>
-{{--                    <div style="max-width: 80%; padding-top: 10px;">dslajf ldsajf; jasl;f jlksa jfdjdsaf lkjdsa f;lkdsajf ;jdsaf ;lks jfda;sjaf;l dsaf</div>--}}
-                </td>
-                <?php $returnamt = 0; $returnamt = $customer->paid_amount - $customer->amount; ?>
-                <td style="border-top:1px solid;"><strong>Total Amount :</strong></td>
-                <td style="border-top:1px solid;"><strong>{{getIndianCurrency($totalamt - $customer->discount_amount)}}</strong></td>
-            </tr>
-            @endif
-            <tr>
-                <td colspan="3" rowspan="1" style="border-top:1px solid; text-align: left;">
-                    <div><strong style="border-bottom: 1px solid;"></strong></div>
-                    <div style="padding-top: 10px;"></div>
-{{--                    <div style="max-width: 80%; padding-top: 10px;">dslajf ldsajf; jasl;f jlksa jfdjdsaf lkjdsa f;lkdsajf ;jdsaf ;lks jfda;sjaf;l dsaf</div>--}}
-                </td>
-
-                <td style="border-top:1px solid;"><strong>Paid Amount :</strong></td>
-                <td style="border-top:1px solid;"><strong>{{$customer->paid_amount}}</strong></td>
-            </tr>
-            @if($customer->paid_amount)
-            <tr>
-                <td colspan="3" rowspan="1" style="border-top:1px solid; text-align: left;">
-                    <div><strong style="border-bottom: 1px solid;"></strong></div>
-                    <div style="padding-top: 10px;"></div>
-{{--                    <div style="max-width: 80%; padding-top: 10px;">dslajf ldsajf; jasl;f jlksa jfdjdsaf lkjdsa f;lkdsajf ;jdsaf ;lks jfda;sjaf;l dsaf</div>--}}
-                </td>
-
-                <td style="border-top:1px solid;"><strong>Change Amount :</strong></td>
-                <td style="border-top:1px solid;"><strong>{{round($customer->paid_amount - $customer->amount)}}</strong></td>
-            </tr>
-            @endif
-
-        </table>
-
-        <table style="width: 100%;">
-            <tr>
-                <td style="height: 60px;"></td>
-            </tr>
-            <tr>
-                <td style="text-align: right;">
-                Medicines once sold will not be taken back or exchanged.
-                </td>
-            </tr>
-        </table>
-        <div>
-            <button onclick="window.print()" class="btn no-print btn-lg btn-info printBtn">Print <i class="fas fa-print"></i></button>
-        </div>
-    </div>
-    @endsection
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script>
-        // $('.printBtn').click(function(){
-        //     $(".printBtn").show().delay(5000).fadeOut();
-        // });
-    </script>
+          <!-- <td style="text-align: right;">
+            (Authorized Signatory)
+          </td> -->
+        </tr>
+      </table>
+	  <div>
+	  <input type="button" id="printPageButton" value="Print this page" onClick="window.print()">
+      </div>
+	</div>
+  </body>
+</html>
